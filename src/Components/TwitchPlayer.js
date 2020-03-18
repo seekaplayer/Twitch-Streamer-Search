0bf568fc7143c4cf0d95fd003c103c9fc156867c
Seekaplayer <44986525+seekaplayer@users.noreply.github.com>
2020-03-18 01:25:00 -0400

    Fixed video width and size on desktop to mobile

--- src/Components/TwitchPlayer.js
+++ src/Components/TwitchPlayer.js
@@ -2,15 +2,19 @@ import React, { useEffect } from "react";
 const TwitchPlayer = ({ username }) => {
   useEffect(() => {
     var options = {
-      width: 854,
-      height: 300,
       channel: username
     };
     var player = new window.Twitch.Player("twitchPlayerJS", options);
     player.setVolume(0.5);
   }, [username]);
 
-  return <div id="twitchPlayerJS"></div>;
+  return (
+    <>
+      <div className="twitchPlayer">
+        <div id="twitchPlayerJS"></div>
+      </div>
+    </>
+  );
 };
 
 export default TwitchPlayer;
