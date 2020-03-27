cfea3cd91ac26a88720a118cccd08a35f7c1cf65
Seekaplayer <44986525+seekaplayer@users.noreply.github.com>
2020-03-27 00:21:31 -0400

    added copyright info

--- src/Components/Templates/Footer.js
+++ src/Components/Templates/Footer.js
@@ -3,7 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="text-center mt-5">
-      &copy; {new Date().getFullYear()} Streamer Search, All Rights Reserved.
+      &copy; {new Date().getFullYear()} Streamer Search, Created by{" "}
+      <a href="https://seekaplayer.com">Seekaplayer</a> All Rights Reserved.
     </footer>
   );
 };
